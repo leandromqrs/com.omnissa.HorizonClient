14f05024f7d7f32e08f7f0f079d1f6a25c36f2df
Leandro Rodrigues <leandromqrs@hotmail.com>
2026-04-29 11:15:18 -0300

    Add Flatpak build and release workflow, update YAML configurations, and replace VMware references with Omnissa

--- dev/install.sh
+++ dev/install.sh
@@ -1,4 +1,4 @@
-flatpak install org.gnome.Sdk/x86_64/42
-flatpak install org.gnome.Platform/x86_64/42
+flatpak install org.gnome.Sdk/x86_64/50
+flatpak install org.gnome.Platform/x86_64/50
 
-flatpak-builder --user --install --force-clean build-dir com.vmware.HorizonClient.yaml
+flatpak-builder --user --install --force-clean build-dir com.omnissa.HorizonClient.yaml
